b8c2f6a6e5bc51443f1f110a5ea0df90f29fcc46
EthanShapiro <23350520+EthanShapiro@users.noreply.github.com>
2017-09-27 20:55:33 -0700

    Test VCS

--- FPS-Game/Assets/characterAnimation.cs
+++ FPS-Game/Assets/characterAnimation.cs
@@ -3,40 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 
-public class characterAnimation : MonoBehaviour {
-
-    public GameObject FPSController;
-
+public class characterAnimation : MonoBehaviour
+{
     private CharacterController charcontroller;
 
     Vector3 horizontalvelocity;
 
     Animator chanimator;
 
-    Rigidbody rb;
-
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 
         charcontroller = GetComponentInParent<CharacterController>();
 
         chanimator = this.GetComponent<Animator>();
-
-        rb = this.GetComponentInParent<Rigidbody>();
-
-     
- 
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
+	void Update ()
+    {
         horizontalvelocity = new Vector3(charcontroller.velocity.x, 0, charcontroller.velocity.z);
 
         float horizontalspeed = horizontalvelocity.magnitude;
 
         chanimator.SetFloat("Speed_f", horizontalspeed);
-
 
         if (charcontroller.velocity.y >= 0.1)
         {
@@ -48,7 +39,5 @@ public class characterAnimation : MonoBehaviour {
         {
             chanimator.SetBool("Jump_b", false);
         }
-
-
     }
 }
